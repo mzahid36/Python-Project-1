7f5b0ad91469dfc20b094146e99bb29af518c275
Masud Zahid <zahidul15-12522@diu.edu.bd>
2022-12-11 16:07:35 +0600

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This section of code only for decoration purpose.
+print()
+print('-'*40)
+print('-','\tProject 1 : Alarm Clock',' '*6,'-')
+print('-'*40,'\n\n')
+#decoration ends here
+
+#user input section (Hour - Minutes - am/pm)
+hour = int(input('Enter Hour : '))
+minutes = int(input('Enter Minutes : '))
+am_pm = input('am / pm : ')
