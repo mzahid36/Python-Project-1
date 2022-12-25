4aaa7e31f9a2377bfd5ef0140c5e82dc6612ef78
Masud Zahid <zahidul15-12522@diu.edu.bd>
2022-12-25 13:41:05 +0600

    try_catch

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+import datetime
+
 # This section of code only for decoration purpose.
 print()
 print('-'*40)
 print('-','\tProject 1 : Alarm Clock',' '*6,'-')
 print('-'*40,'\n\n')
 #decoration ends here
+try:
+    #user input section (Hour - Minutes - am/pm)
+    print('--Time format 12h.--\n')
+    hour = int(input('Enter Hour : '))
+    minutes = int(input('Enter Minutes : '))
+    am_pm = input('am / pm : ')
 
-#user input section (Hour - Minutes - am/pm)
-hour = int(input('Enter Hour : '))
-minutes = int(input('Enter Minutes : '))
-am_pm = input('am / pm : ')
+    if (hour > 12 or hour < 0) or (minutes > 60 or minutes < 0) or (am_pm != 'am' and am_pm != 'pm'):
+        # print('Please enter correct time.') 
+        raise Exception
+    if am_pm == 'pm':
+        hour += 12
 
-if (hour > 12 or hour < 0) or (minutes > 60 or minutes < 0 ) or ( am_pm != 'am' and am_pm != 'pm'):
-    print('Please enter correct time.') 
+except Exception:
+    print('\nFound Error: Please enter correct time.\n')
+
+else:
+    print('\nYour alarm time is - ',hour,':',minutes,':',am_pm)
 
